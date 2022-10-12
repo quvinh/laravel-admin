f3bef6fcc805ac606932a42e24d649c62d767297
quvinh <vinhhp2620@gmail.com>
2022-10-12 23:29:37 +0700

    first commit

---
+++ app/Admin/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Admin\Controllers\HomeController;
+use Encore\Admin\Facades\Admin;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', [HomeController::class, 'index'])->name('home');
+    $router->get('/calendar', [HomeController::class, 'calendar'])->name('calendar');
+});
